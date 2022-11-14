06114fa68ef5623e11e91fd73626b94f52915d9d
0x000013 <118074602+Mas01x@users.noreply.github.com>
2022-11-14 12:10:07 -0500

    Create AdvikIsWierd.java

---
+++ AdvikIsWierd.java
@@ -0,0 +1,8 @@
+public class MyFirstJavaProgram {
+   public static void main(String []args) {
+     bool isAdvikWierd = true;
+     if (isAdvikWierd) {
+        System.out.println("Advik Is Wierd");
+     }
+   }
+}
